ee84bcd87080af12e75268999a1318c638ab52e1
VITORIA NUNES FARIAS <05563049000@senacrs.edu.br>
2026-03-23 17:14:06 -0300

    Operadores

---
+++ Operadores/OperadoresAritmeticos/Conteudo.js
@@ -0,0 +1,59 @@
+///SOMA:
+const primeiroValor = 10
+const segundoValor = 20
+const resultado = primeiroValor + segundoValor + 4
+
+console.log(resultado)
+
+///SUBTRAÇÃO:
+const primeiroValor2 = 10
+const segundoValor2 = 20
+const resultado2 = primeiroValor2 - segundoValor2
+
+console.log(resultado2)
+
+///MULTIPLICAÇÃO:
+const primeiroValor3 = 10
+const segundoValor3 = 25
+const resultado3 = primeiroValor3 * segundoValor3
+
+console.log(resultado3)
+
+///DIVISÃO
+const primeiroValor4 = 345
+const segundoValor4 = 10
+const resultado4 = primeiroValor4 / segundoValor4
+
+console.log(resultado4)
+
+///COMPARADORES
+//igual: ===
+//diferente: !==
+//maior e menor igual: > e >=
+//menor e menor igual: < e <=
+
+/// === VERIFICA SE O VALOR E O TIPO SÃO IGUAIS
+"1" === "2" // false, são diferentes
+"2" === "2" // true, são iguais
+2 === "2" // false, tipo diferente
+
+const condição = 1 === 2
+// o valor que sai da comparação
+// pode ser guardado em uma variavel
+// nesse caso, candição === false
+
+/// !== VERIFICA SE O VALOR E O TIPO SÃO DIFERENTES
+"1" !== "2" // true, são diferentes
+"2" !== "2" // false, são iguais
+2 !== "2" // true, tipos diferentes
+
+const condição = 1 !==  // true
+
+/// > e >
+1 > 2 // false, porque 1 é menor que 2
+2 > 2 // false, porque 2 é igual a 2
+3 > 2 // true, porque 3 é maior que 2
+
+1 >= 2 // false, porque 1 é menor que 2
+2 >= 2 // true, porque 2 é igual a 2
+3 >= 2 // true, porque 3 é maior que 2 
---
+++ Operadores/exercicio2.js
@@ -0,0 +1,6 @@
+const primeiroValor = 15
+const segundoValor = 15
+const terceiroValor = 10
+const resultado = primeiroValor + segundoValor + terceiroValor
+
+console.log(resultado)
