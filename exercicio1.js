b66f73e65144e0b0c701cdc95d54884316cbca1a
Vitória Nunes Farias <vitorianunesfarias82@gmail.com>
2026-04-08 16:36:44 -0300

    Delete exercicio1.js

--- exercicio1.js
+++
@@ -1,7 +0,0 @@
-const prompt = require("prompt-sync")()
-
-let nome = prompt("Qual seu nome? ")
-let idade = prompt("Qual sua idade? ")
-let cidade = prompt("Onde você mora? ")
-
-console.log(`Olá ${nome} você tem ${idade} anos de idade e mora em ${cidade}, correto?`)
