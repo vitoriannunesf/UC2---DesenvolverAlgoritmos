db4e1fed4130eacf9892d30c1c096d1d29d7e653
Vitória Nunes Farias <vitorianunesfarias82@gmail.com>
2026-04-13 14:49:40 -0300

    Add files via upload

---
+++ StringEArrays/exercicio5Array.js
@@ -0,0 +1,9 @@
+const listaIngredientes = ["ovos" , "farinha de trigo" , "açúcar" , "manteiga" , "leite", "chocolate em pó" , "fermento" , "leite condensado" , "nutella" , "creme de leite" , "chantilly" , "leite ninho"]
+const baseDoBolo = ["farinha" , "açúcar" , "ovos" ,"manteiga" , "leite" , "fermento"]
+let recheio1 = ["brigadeiro"]
+let recheio2 = ["nutella"]
+let recheio3 = ["leite ninho"]
+
+const boloPronto = [baseDoBolo , recheio1 , baseDoBolo , recheio2 , baseDoBolo , recheio3 , baseDoBolo]
+
+console.log(boloPronto) 
