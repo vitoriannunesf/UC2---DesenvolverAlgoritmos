0419af40a12999287662cdd9763ae0c3fa84ee1b
Vitória Nunes Farias <vitorianunesfarias82@gmail.com>
2026-04-14 17:14:12 -0300

    Add files via upload

--- Funções/TiposFuncoes.js
+++ Funções/TiposFuncoes.js
@@ -13,3 +13,16 @@ function imprimirOlaMundo(){
 
 // Chamada da função para executar 
 imprimirOlaMundo()
+
+//------------------------------------------
+// 2. Função com parâmetros
+// Podemos passar informações para a função. Essas informações são os parâmetros que ela vai precisar para executar sua tarefa
+
+//------------------------------------------
+// Função que soma dois números:
+function somar(a,b){
+    return a + b
+}
+// Chamada da função:
+let resultado = somar(5,3)
+console.log(resultado)
