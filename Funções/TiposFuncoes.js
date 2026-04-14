dfcdee0b5c3c5672f62b4582483a86bc21e216e1
Vitória Nunes Farias <vitorianunesfarias82@gmail.com>
2026-04-14 17:04:18 -0300

    Add files via upload

---
+++ Funções/TiposFuncoes.js
@@ -0,0 +1,15 @@
+// Funções:
+// Uma função é um bloco de código qeu executa uma tarefa específica.
+// Recebe algo(entrada ou parâmetros)
+// Processa algo
+// Entrega um resulatado (saída ou retorno)
+
+//-------------------------------------------
+// 1. Função Declarada (padrão/sem parâmetro)
+// Declarando a função
+function imprimirOlaMundo(){
+    console.log("Olá mundo!") // ação da função
+}
+
+// Chamada da função para executar 
+imprimirOlaMundo()
